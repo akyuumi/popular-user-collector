720e208085e06037b856e65ae935d2c483bf951e
naruke <akyuumix@gmail.com>
2025-06-07 21:38:21 +0900

    gitignoreを追加

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
 INSTAGRAM_ACCESS_TOKEN = ""
 
 # X (Twitter) API credentials
-X_API_KEY = os.getenv('X_API_KEY')
+X_API_KEY = os.getenv('xx-xx')
 X_API_SECRET = os.getenv('X_API_SECRET')
 X_ACCESS_TOKEN = os.getenv('X_ACCESS_TOKEN')
 X_ACCESS_TOKEN_SECRET = os.getenv('X_ACCESS_TOKEN_SECRET')
