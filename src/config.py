069d4efae8ca11742fc59dccefbc9e4a04d61a12
naruke <akyuumix@gmail.com>
2025-06-08 16:13:21 +0900

    gitignoreを修正

---
+++ src/config.py
@@ -0,0 +1,14 @@
+import os
+
+# Google Cloudコンソールで取得したAPIキーを設定(export YOUTUBE_API_KEY=YOUR_API_KEY)
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
+
+# Instagram API Key
+INSTAGRAM_ACCESS_TOKEN = ""
+
+# X (Twitter) API credentials
+X_API_KEY = os.getenv('X_API_KEY')
+X_API_SECRET = os.getenv('X_API_SECRET')
+X_ACCESS_TOKEN = os.getenv('X_ACCESS_TOKEN')
+X_ACCESS_TOKEN_SECRET = os.getenv('X_ACCESS_TOKEN_SECRET')
+X_BEARER_TOKEN = os.getenv('X_BEARER_TOKEN')
